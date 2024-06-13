6c22756b1121237da97575b0558c2fcb8476f3f3
jarne <julien@jusdeliens.com>
2024-06-14 00:11:50 +0200

    Added mosquitto config files, README and ovahub script. Works well on windows host. No acls implemented yet

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # ovahub
+MQTT Broker tools for administrating Ova bots and Jusdeliens IDEAL games
+
+## 🎯 Context
+
+`OvaHub` is a CLI tool to allow any user to administrate its own mqtt broker on a **L**ocal **A**rea **N**etwork to play with Jusdeliens Ova bots and IDEAL games.
+
+## 📁 Project structure
+
+- **.vscode** : IDE settings tasks to be loaded in VScode using `CTRL+SHIFT+B` shortcut
+- **mosquitto** : Broker folder with all configurations files, logs and store data
+    - **config** : Contains the `acls.ini` `passwd.ini` `mosquitto.conf` file to configure the broker settings
+    - **data** : Stores the data exchanged in a database in `mosquitto.db` once the broker is started
+    - **log** : Where `mosquitto.log` will be filled with debug informations according to the verbosity set in `mosquitto.conf` file
+
+## 🛠️ Setup
+
+To deploy a broker, you need to be on an admin session with the following software installed
+- [Docker Engine](https://www.docker.com/products/docker-desktop/) 
+- as well as [git](https://www.git-scm.com/downloads)
+- and [VSCode](https://code.visualstudio.com/download) IDE
+
+First, clone this project from our github
+```
+git clone https://github.com/jusdeliens/ovahub.git
+```
+
+Then start Docker Engine as administrator. According to your OS, the Docker installation process may be different :
+
+### 🪟 Windows
+
+Install [Docker Desktop]() on a session with admin rights. Also, make sure you have the [WSL2 kernel update](https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi) installed on your windows. If not, you will have to install it at the end of Docker Desktop installation. 
+Click here if you want to [know more about WSL](https://learn.microsoft.com/fr-fr/windows/wsl/install).
+
+### 🍏 Unix
+
+[Follow theses steps](https://docs.docker.com/desktop/install/linux-install/) to install Docker Desktop whether you are on Ubuntu, Debian, Fedora or Arch.
+
+## 🚀 Deploy
+
+Once Docker Desktop started on your admin session, follow theses steps
+
+### 🔑 Create users and password 
+When using Jusdeliens product, you should have at least 2 users :
+- `demo` : for non admin users
+- `admin` : only for 1 admin user
+
+Usernames and password must be stored encrypted in the `passwd.ini` file. To do it, simply use `CTRL+SHIFT+B` shortcut in VSCode and select the task `Register users and passwords`.
+
+Otherwise, you can use the following command in a elevated shell (`gitbash` for instance) in the project dir :
+
+```
+python ovahub.py regusr
+```
+
+This will ask you in the console the username and password for each user to add. Once the task is complete, the passwd file will contain per line the username followed by `:` then the encrypted password
+
+⚠️ Carefull, when running this command, all previous users stored in `passwd.ini` are deleted. 
+
+### 🐳 Start the broker 
+
+Once the user created, let's run the broker on your LAN ! use `CTRL+SHIFT+B` shortcut in VSCode and select the task `Start broker in a local container`, or use the following command :
+
+```
+python ovahub.py start
+```
+
+This should start the broker and display your network informations to give to user so that they could connect to your broker :
+
+```
+🟢 The broker may now be online on LAN at 192.168.x.x, with following available ports for clients 👇
+📬 mqtt: 1883
+📬 ws: 9001
+If not, check your network connection and your firewall
+To join/admin an arena, open this url your web browser
+👉 http://play.jusdeliens.com/login/?arena=BattlefX&viewer=tactx&url=192.168.x.x&port=9001&show=address_port_username_password_viewer_arena_pseudo
+```
+
+then, this kind of successfull logs
+
+```
+1718315757: mosquitto version 2.0.18 starting
+1718315757: Config loaded from /mosquitto/config/mosquitto.conf.
+1718315757: Opening ipv4 listen socket on port 1883.
+1718315757: Opening ipv6 listen socket on port 1883.
+1718315757: Opening websockets listen socket on port 9001.
+1718315757: mosquitto version 2.0.18 running
+```
+
+If not, check 
+- your firewall so that it allows the mqtt and ws port to be opened
+- your network connection  
+
+Now you can fully enjoy playing with Ova bots and IDEAL games without any constraints. Feel free to administrate your broker as you want by changing `acls.ini` `mosquitto.conf` and `passwd.ini`. Go on the [eclipse manual page](https://mosquitto.org/man/mosquitto-conf-5.html) to know more about how to do it.
+
+If you need more help, feel free to [contact us](https://jusdeliens.com/contact) to join our robotic masterclasses 🤖
+
+## 🧑‍💻 Author
+Designed with 💖 by [Jusdeliens Inc.](https://jusdeliens.com)
+
+## ⚖️ Licence
+Under [CC BY-NC 4.0 licence](https://creativecommons.org/licenses/by-nc/4.0/deed.en) 
---
+++ ovahub.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+#                           ██╗██████╗ ██╗           
+#                           ██║╚════██╗██║           
+#                           ██║ █████╔╝██║           
+#                      ██   ██║██╔═══╝ ██║           
+#                      ╚█████╔╝███████╗███████╗      
+#                       ╚════╝ ╚══════╝╚══════╝      
+#                       https://jusdeliens.com
+#
+# Designed with 💖 by Jusdeliens
+# Under CC BY-NC 4.0 licence
+# https://creativecommons.org/licenses/by-nc/4.0/deed.en
+#  
+# Be sure to use a unix shell (gitbash on Windows)
+
+import os
+import subprocess
+import time
+import sys
+from getpass import getpass
+
+def error(msg, endl="\n"):
+	msg = "🚨 "+msg
+	print(msg, end=endl)
+def warning(msg, endl="\n"):
+	msg = "⚠️  "+msg
+	print(msg, end=endl)
+def info(msg, endl="\n"):
+	print(msg, end=endl)
+def ospathjoin(path:str, *paths):
+	return os.path.join(path, *paths)
+def getWorkingDir():
+	return os.getcwd()
+
+
+config = {
+	"config-dir": [ospathjoin(getWorkingDir(), "mosquitto", "config"), 
+		"The mosquitto dir where to find conf.ini"],
+	"data-dir": [ospathjoin(getWorkingDir(), "mosquitto", "data"), 
+		"The mosquitto dir where to find database backup"],
+	"log-dir": [ospathjoin(getWorkingDir(), "mosquitto", "log"), 
+		"The mosquitto dir where to find logs"],
+	"name": ["ovahub",
+		"The name of the docker container running the broker"],
+	"mqtt-port": ["1883", 
+	    "The default port for mqtt clients"], 
+	"ws-port": ["9001",
+		"The websocket port for web browser clients"] ,
+	"img": ["eclipse-mosquitto:latest", 
+	 	"The broker image to download from docker repo"],
+	"os": ["win", 
+		"The host machine os. 'win' = windows, 'unix' = linux, mac ... "],
+	"arena": ["BattlefX", 
+		"The name of the default arena to join "],
+	"viewer": ["tactx", 
+		"The of the html folder on viewer website. Can be 'robots','tactx','xihara'"]
+}
+
+def run(cmd, sudo=False, targetOS=config["os"][0], debug=True):
+	"""Run a cmd in the actual bash"""
+	if ( sudo and targetOS=="unix"):
+		cmd = "sudo -S -p '' "+ cmd
+	if debug:
+		info("> "+cmd)
+	subprocess.call(cmd, shell=True)
+
+def help():
+	"""Display the manual with all options"""
+	print()
+	print("Usage: python3 ovahub.py COMMAND [OPTION]")
+	print()
+	print("Administrate your own LAN mqtt broker in a unix shell to play with Jusdeliens Ova bots and IDEAL games")
+	print()
+	print("Sample:")
+	print("\tTo start mqtt broker container listening 1883 and 9001 port on a windows host machine")
+	print("\t> python ovahub.py start --mqtt-port 1883 --ws-port 9001 --os win")
+	print()
+	print("Options:")
+	for key,value in config.items():
+		print(f"\t--{key}")
+		print(f"\t\t{value[1]}")
+		print(f"\t\tdefault: '{value[0]}'")
+		print()
+	print()
+	print("Commands:")
+	for key,value in cmd.items():
+		print(f"\t{key}\t{value[1]}")
+	exit()
+
+def stop():
+	"""Stop and remove the broker container"""
+	containersToStop = [config["name"][0]]
+	for containerName in containersToStop:
+		try:
+			info("⏳ Clearing old container and image ... ")
+			cmds = ["docker stop "+containerName, "docker rm "+containerName]
+			for cmd in cmds:
+				run(cmd, sudo=True)
+		except Exception as e:
+			error("Fail stopping docker"+": "+str(e))
+			raise SystemExit
+
+def clear():
+	"""Clearing old logs"""
+	dirsToRemove = []
+	filesToRemove = [ospathjoin(config["log-dir"][0],"*.log")]
+	for dirToRemove in dirsToRemove:
+		info("⏳ Removing build dir "+dirToRemove+"... ", endl="\n")
+		try:
+			run("rm -rf "+dirToRemove, sudo=True)
+			info("✅")
+		except Exception as e:
+			info("❌")
+			warning("Fail to remove build dir "+dirToRemove+""+": "+str(e))
+	for fileToRemove in filesToRemove:
+		info("⏳ Removing build file "+fileToRemove+"... ", endl="\n")
+		try:
+			run("rm "+fileToRemove, sudo=True)
+			info("✅")
+		except Exception as e:
+			info("❌")
+			warning("Fail to remove file "+fileToRemove+""+": "+str(e))
+		
+def start():
+	"""Called when 'start' command is specified"""
+	try:
+		stop()
+		clear()
+	except:
+		...
+	info("⏳ Starting container ... ")
+	run(f'docker run -dit --restart unless-stopped -v {config["config-dir"][0]}/:/mosquitto/config -v {config["data-dir"][0]}/:/mosquitto/data -v {config["log-dir"][0]}/:/mosquitto/log -p {config["mqtt-port"][0]}:1883 -p {config["ws-port"][0]}:9001 --name {config["name"][0]} {config["img"][0]}', sudo=True)
+
+	try:
+		import socket
+		hostname = socket.gethostname()
+		ip_address = socket.gethostbyname(hostname)
+		info("")
+		info(f"🟢 The broker may now be online on LAN at {ip_address}, with following available ports for clients 👇")
+		info(f"📬 mqtt: {config['mqtt-port'][0]}")
+		info(f"📬 ws: {config['ws-port'][0]}")
+		info("If not, check your network connection and your firewall")
+		info(f"To join/admin an arena, open this url your web browser")
+		info(f"👉 http://play.jusdeliens.com/login/?arena={config['arena'][0]}&viewer={config['viewer'][0]}&url={ip_address}&port={config['ws-port'][0]}&show=address_port_username_password_viewer_arena_pseudo")
+		info("")
+		time.sleep(1)
+	except:
+		...
+
+	logfile = ospathjoin(config["log-dir"][0],"mosquitto.log")
+	info(f"⏳ Debuging log from {logfile} ...")
+	info("💡 Press CTRL+C to interrupt debug once started")
+	time.sleep(2)
+	run(f"tail -F {logfile}")
+
+def regusr():
+	pwdfilepath = ospathjoin(config["config-dir"][0],"passwd.ini")
+	with open(pwdfilepath, 'w', encoding='utf-8') as f:
+		print()
+		print("Add new users and password.")
+		print("Press Enter with empty username to stop.")
+		users = {}
+		while True:
+			usrname = input("👤 username: ")
+			if len(usrname) == 0:
+				break
+			while True:
+				print("🔑 password: ")
+				usrpwd = getpass(">")
+				if len(usrpwd) < 4:
+					print("⚠️ Enter a stronger password (>4 characters)")
+				else:
+					break
+			users[usrname] = usrpwd
+		for usrname, usrpwd in users.items():
+			f.write(f"{usrname}:{usrpwd}\n")			
+        # your logic goes right here
+	run(f'docker run -it --rm -v {config["config-dir"][0]}/:/mosquitto/config --name {config["name"][0]}-regusr {config["img"][0]} mosquitto_passwd -U /mosquitto/config/passwd.ini', sudo=True)
+
+
+cmd = {
+	"start": [start, "Start the broker container"],
+	"stop": [stop, "Stop the broker container"],
+	"regusr": [regusr, "Clear all previous users added, then add users and passwords in passwd ini file"],
+	"help": [help, "Display the man page"]
+}
+
+os.system("chcp.com 65001")
+os.system("export PYTHONIOENCODING=utf-8")
+os.system("export LANG=en_US.UTF-8")
+
+nArg = len(sys.argv)
+iArg = 1
+cmdArg = "help"
+while iArg<nArg:
+	argName = sys.argv[iArg]
+	argValue = None
+	if iArg == 1:
+		if argName in cmd:
+			cmdArg = argName
+	elif ( iArg+1 < nArg ):
+		argValue = sys.argv[iArg+1]
+		argKey = argName.replace("--","")
+		if argKey in config:
+			info(f"Changed config '{argName}' from '{config[argKey][0]}' to '{argValue}'")
+			config[argKey][0] = argValue
+			iArg += 1
+	iArg+=1
+cmd[cmdArg][0]()
+
